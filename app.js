997a3236eb076faf59dc4973e16d4cbdd263802d
Gilles Thomas <githoweb@gmail.com>
2025-06-06 19:50:30 +0200

    Simplify and get rid of matchMedia

--- app.js
+++ app.js
@@ -4,29 +4,8 @@ const share = document.querySelector(".share");
 
 let largeur = window.innerWidth;
 const mediaQuery = window.matchMedia("(min-width: 601px)");
-mediaQuery.addEventListener("change", handleChange);
 
 shareButton.addEventListener("click", (e) => {
-  if (window.innerWidth <= 600) {
-    author.classList.toggle("hide");
     share.classList.toggle("hide");
-  } else {
-    share.classList.toggle("hide");
-  }
 });
 
-function handleChange(e) {
-  if (e.matches) {
-    console.log("> 600");
-    if (!share.classList.contains("hide")) {
-      author.classList.remove("hide");
-    }
-  } else {
-    console.log("<= 600");
-    if (!share.classList.contains("hide")) {
-      author.classList.add("hide");
-    }
-  }
-}
-
-console.log("Largeur du viewport :", largeur);
